2522d80706d915b4cf3894921cf55fedfdea8e57
krutagn.codes <thesiyakrutagn@gmail.com>
2026-08-11 11:50:14 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (86.92%) - LeetSync

---
+++ 169-majority-element/majority-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+  let freq = 0;
+  let ans = 0;
+  for(let i =0 ; i < nums.length ; i++){
+    if(freq == 0){
+        ans = nums[i];
+    } if (ans == nums[i]) {
+        freq++;
+    } else {
+        freq--;
+    }
+  }
+  return ans;
+};
